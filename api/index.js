86cfe1a266b1c3f7bdd201ac76b4366e555bb1fc
CodingRuo <r.schober@outlook.com>
2022-07-14 16:32:13 +0200

    pushing to github

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+app.get("/posts", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  let posts = [];
+  db.collection("posts")
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data());
+        posts.push(doc.data());
+      });
+      res.send(posts);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ src/pages/PageCamera.vue
@@ -0,0 +1,225 @@
+<template>
+  <q-page class="constraint-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <video v-show="!imageCaptured" class="full-width" autoplay ref="video" />
+      <canvas
+        v-show="imageCaptured"
+        ref="canvas"
+        class="full-width"
+        height="240"
+      />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn
+        v-if="hasCameraSupport"
+        color="grey-10"
+        icon="eva-camera"
+        round
+        size="lg"
+        @click="captureImage"
+      />
+      <q-file
+        v-else
+        outlined
+        v-model="imageUpload"
+        label="Choose an image"
+        accept="image/*"
+        @input="captureImageFallback"
+      >
+        <template v-slot:prepend>
+          <q-icon name="eva-attach-outline" />
+        </template>
+      </q-file>
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="post.caption"
+          type="text"
+          label="Caption"
+          class="col col-sm-6"
+          dense
+        />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="post.location"
+          type="text"
+          label="Location"
+          class="col col-sm-6"
+          dense
+          :loading="pendingLocation"
+        >
+          <template v-slot:append>
+            <q-btn
+              v-if="!pendingLocation && locationSupported"
+              @click="getLocation"
+              round
+              dense
+              flat
+              icon="eva-navigation-2-outline"
+            />
+          </template>
+        </q-input>
+      </div>
+      <div class="row justify-center q-mt-lg">
+        <q-btn color="primary" unelevated rounded label="Post Image" />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { uid } from "quasar";
+require("md-gum-polyfill");
+
+export default {
+  name: "PageCamera",
+  data() {
+    return {
+      post: {
+        id: uid(),
+        caption: "",
+        location: "",
+        photo: null,
+        date: Date.now(),
+      },
+      imageCaptured: false,
+      hasCameraSupport: true,
+      imageUpload: [],
+      pendingLocation: false,
+    };
+  },
+  computed: {
+    locationSupported() {
+      if ("geolocation" in navigator) {
+        return true;
+      }
+      return false;
+    },
+  },
+  methods: {
+    initCamera() {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true,
+        })
+        .then((stream) => {
+          this.$refs.video.srcObject = stream;
+        })
+        .catch((error) => {
+          this.hasCameraSupport = false;
+        });
+    },
+    captureImage() {
+      const canvas = this.$refs.canvas;
+      const video = this.$refs.video;
+      canvas.width = video.getBoundingClientRect().width;
+      canvas.height = video.getBoundingClientRect().height;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.imageCaptured = true;
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL("image/png"));
+      this.disableCamera();
+    },
+    captureImageFallback(file) {
+      this.post.photo = file;
+
+      const canvas = this.$refs.canvas;
+      const context = canvas.getContext("2d");
+
+      var reader = new FileReader();
+      reader.onload = (event) => {
+        var img = new Image();
+        img.onload = () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img, 0, 0);
+          this.imageCaptured = true;
+        };
+        img.src = event.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    disableCamera() {
+      this.$refs.video.srcObject.getTracks().forEach((track) => {
+        track.stop();
+      });
+    },
+    dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(",")[1]);
+
+      // separate out the mime component
+      var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      var ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], { type: mimeString });
+      return blob;
+    },
+    getLocation() {
+      this.pendingLocation = true;
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            this.getCityCountry(position);
+          },
+          (err) => {
+            this.locationError();
+          },
+          { timeout: 7000 }
+        );
+      }
+    },
+    getCityCountry(position) {
+      let apiURL = `https://geocode.xyz/${position.coords.latitude},${position.coords.longitude}?json=1`;
+      this.$axios
+        .get(apiURL)
+        .then((response) => {
+          this.locationSuccess(response);
+        })
+        .catch((error) => {
+          this.locationError();
+        });
+    },
+    locationSuccess(result) {
+      this.post.location = result.data.city;
+      if (result.data.country) {
+        this.post.location += `, ${result.data.country}`;
+      }
+      this.pendingLocation = false;
+    },
+    locationError() {
+      this.pendingLocation = false;
+      this.$q.dialog({
+        title: "Error",
+        message: "Could not find your location",
+      });
+    },
+  },
+  mounted() {
+    this.initCamera();
+  },
+  beforeDestroy() {
+    if (this.hasCameraSupport) {
+      this.disableCamera();
+    }
+  },
+};
+</script>
+
+<style lang="sass">
+.camera-frame
+  border: 2px solid $grey-10
+  border-radius: 10px
+</style>
